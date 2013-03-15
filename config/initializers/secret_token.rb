0d594db74f296caa293f1b18a0ecf41147a602bd
Jonathan Wexler <Jwexman@gmail.com>
2013-03-15 16:30:43 -0400

    Initial Commit
    Created new App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zambazu::Application.config.secret_token = '42bf629b11ccefd8e81b73c10e4e8e7901fc33b12bad771d272b8cf82dc6ed2ba39a201ba8a89013b4308d1ac6b8c08a0d7dc0cb6513779a5e5040d1092e458c'
